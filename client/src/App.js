5854849a616252ec8b8ad199cf325d3e7ff95349
Sahil Mody <sahilmody1234@gmail.com>
2023-02-24 19:38:10 -0500

    Created form Added redux operations

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+import {useDispatch} from 'react-redux'
+import {getPosts} from './actions/posts'
 import memories from './images/memories.png'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
@@ -7,6 +10,15 @@ import useStyles from './styles'
 
 const App = () => {
     const classes =useStyles()
+    const dispatch = useDispatch()
+
+    React.useEffect(()=>{
+        dispatch(getPosts())
+    },[dispatch])
+
+
+
+
     return (
         <Container maxWidth ='lg'>
             <AppBar className={classes.appBar} position="static" color="inherit">
@@ -15,7 +27,7 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify ='space-between' alignItems="stretch" spacing={3}>
+                    <Grid container justifyContent ='space-between' alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts />
                         </Grid>
